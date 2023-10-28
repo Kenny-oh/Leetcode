fae87e90169ede70051061ac141f2f8f4f04d0f1
Kennyinuskoh <142675325+Kennyinuskoh@users.noreply.github.com>
2023-10-28 00:57:59 -0700

    addition of folders for hackerrank and codewar
    2 leetcode questions 
    hackerrank folder
    codewar folder

---
+++ Project 13/LeetCode/two-sums.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const numMap = new Map();
+    for (i=0; i< nums.length; i++) {
+        const complement = target -nums[i];
+        if (numMap.has(complement)) {
+            return [numMap.get(complement), i];
+        }
+        numMap.set(nums[i], i);
+    }
+    return[];
+};
